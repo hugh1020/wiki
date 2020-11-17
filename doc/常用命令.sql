9bfee9279accd3e5b3e45371d7f8a55f42aadb8c
hugh <twtwhte@163.com>
2020-11-17 11:06:32 +0800

    x

--- doc/常用命令.sql
+++ doc/常用命令.sql
@@ -48,5 +48,22 @@ load data local inpath '/home/pubuser/xxu/tmp01.csv' into table tmp01 part
 
 --------------------------------------------------
 
-   alter table p_emp drop if exists partition  (date_day_p='2020-10-13');
-  alter table p_emp add partition (date_day_p='2020-10-13');
+--检查分区数据
+hadoop fs -ls /user/hive/warehouse/exporder.db/dws_exporder_ordermileage_company_hour/date_month_p=2020-10/date_day_p=2020-10-20
+hadoop fs -ls /user/hive/warehouse/exporder.db/dws_exporder_ordermileage_company_hour/date_month_p=2020-10/date_day_p=2020-10-21
+--从生产拉取指定分区
+hadoop fs -get /user/hive/warehouse/exporder.db/dws_exporder_ordermileage_company_hour/date_month_p=2020-10/date_day_p=2020-10-20/* /home/pubuser/v3copyhive/dws_exporder_ordermileage_company_hour/date_month_p=2020-10/date_day_p=2020-10-20
+hadoop fs -get /user/hive/warehouse/exporder.db/dws_exporder_ordermileage_company_hour/date_month_p=2020-10/date_day_p=2020-10-21/* /home/pubuser/v3copyhive/dws_exporder_ordermileage_company_hour/date_month_p=2020-10/date_day_p=2020-10-21
+
+--在测试表先创建分区
+alter table exporder.dws_exporder_ordermileage_company_hour add partition(date_month_p='2020-10',date_day_p='2020-10-20')
+alter table exporder.dws_exporder_ordermileage_company_hour add partition(date_month_p='2020-10',date_day_p='2020-10-21')
+
+  hadoop fs -ls /user/hive/warehouse/exporder.db/dws_exporder_ordermileage_company_hour/date_month_p=2020-10/date_day_p=2020-10-20
+
+--将数据上传至 分区目录
+hadoop fs -put /home/pubuser/xxu/data/dws_exporder_ordermileage_company_hour/date_month_p=2020-10/date_day_p=2020-10-20/* /user/hive/warehouse/exporder.db/dws_exporder_ordermileage_company_hour/date_month_p=2020-10/date_day_p=2020-10-20
+hadoop fs -put /home/pubuser/xxu/data/dws_exporder_ordermileage_company_hour/date_month_p=2020-10/date_day_p=2020-10-21/* /user/hive/warehouse/exporder.db/dws_exporder_ordermileage_company_hour/date_month_p=2020-10/date_day_p=2020-10-21
+
+-- 检查，count() 失效，select 是正常
+select * from dws_exporder_ordermileage_company_hour where date_day_p='2020-10-20' limit 10;
